a35c9cb518bf29fc05f33d970670802065470ee6
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-08-19 18:10:12 +0200

    Drawer

--- game.py
+++ game.py
@@ -1,13 +1,18 @@
 import pygame
 from App.Event import Event
+from App.Drawer import Drawer
 
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('The Snake Game')
 
-# pygame.display.update()
-
 while True:
     for event in pygame.event.get():
         Event(pygame, event).handle()
+
+    drawer = Drawer(pygame, screen)
+    drawer.background()
+    drawer.head()
+
+    pygame.display.update()

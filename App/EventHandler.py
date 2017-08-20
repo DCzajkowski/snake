23ac85c21fb485be38901c10ce9986071b548cb2
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-08-21 00:32:54 +0200

    Simplify

--- App/EventHandler.py
+++ App/EventHandler.py
@@ -1,31 +1,28 @@
 from config import *
 from pygame.locals import *
-from App.Handler import Handler
 
-class Event:
+class EventHandler:
     event = None
-    handler = None
     game = None
 
     def __init__(self, game, event):
-        self.handler = Handler(game)
         self.event = event
         self.game = game
 
     def handle(self, scene):
         if self.event.type == QUIT:
-            self.handler.quit()
+            self.game.quit()
         if self.event.type == KEYDOWN:
             self.keyPressed(self.event.key, self.event.mod, scene)
 
     def keyPressed(self, key, modifier, scene):
         if self.cmdPressed(modifier):
             if key == K_w:
-                self.handler.quit()
+                self.game.quit()
 
         if scene == GAME_SCENE:
             if key in [K_LEFT, K_RIGHT, K_UP, K_DOWN]:
-                self.handler.moveSnake(key)
+                self.game.moveSnake(key)
             if key == K_ESCAPE:
                 self.game.pause()
             if self.game.inDebugMode():
@@ -48,13 +45,17 @@ class Event:
             if key == K_SPACE:
                 self.game.unpause()
             if key == K_q:
-                self.handler.quit()
+                self.game.quit()
         elif scene == GAME_OVER_SCENE:
             if key == K_ESCAPE:
-                self.handler.quit()
+                self.game.quit()
             if key == K_SPACE:
                 self.game.reset()
                 self.game.run()
+
+    # ---
+    # Modifiers
+    # ---
 
     def cmdPressed(self, modifier):
         return modifier in [KMOD_LMETA, KMOD_RMETA, KMOD_META]
--- App/Game.py
+++ App/Game.py
@@ -1,6 +1,5 @@
 from config import *
-from App.Handler import Handler
-from App.Event import Event
+from App.EventHandler import EventHandler
 from App.Screen import Screen
 from App.Apple import Apple
 import time
@@ -9,7 +8,6 @@ import random
 
 class Game:
     pygame = None
-    handler = None
     display = None
     snake = None
     clock = None
@@ -19,12 +17,11 @@ class Game:
     highscore = None
     scene = GAME_SCENE
 
-    def __init__(self, pygame, snake, highscore = None, display = None, clock = None, handler = None, width = 800, height = 600):
+    def __init__(self, pygame, snake, highscore = None, display = None, clock = None, width = 800, height = 600):
         self.pygame = pygame
         self.snake = snake
         self.display = display if display is not None else pygame.display.set_mode((width, height))
         self.clock = clock if clock is not None else pygame.time.Clock()
-        self.handler = handler if handler is not None else Handler(self)
         self.highscore = highscore if highscore is not None else 0
 
         self.pygame.display.set_caption('The Snake Game')
@@ -55,28 +52,78 @@ class Game:
         }
         self.screen = Screen(self)
 
+    def run(self):
+        self.generateNewApple()
+
+        while True:
+            for event in self.pygame.event.get():
+                EventHandler(self, event).handle(self.scene)
+
+            if self.scene == GAME_OVER_SCENE:
+                self.showGameOverScene()
+            elif self.scene == MENU_SCENE:
+                self.showMainMenuScene()
+            elif self.scene == PAUSE_SCENE:
+                self.showPauseScene()
+            elif self.scene == GAME_SCENE:
+                self.showGameScene()
+
+    # ---
+    # Scenes
+    # ---
+
+    def showMainMenuScene(self):
+        pass
+
+    def showPauseScene(self):
+        self.display.fill(COLOR_ASBESTOS)
+        self.message('Paused', 'Press ESC or space bar to continue... Q quits the game.', COLOR_CLOUDS, COLOR_CLOUDS)
+
+    def showGameOverScene(self):
+        self.display.fill(COLOR_EMERALD)
+        self.message('Game Over (Your Score: ' + str(self.snake.length) + ')', 'Press ESC to quit or space bar to continue...', COLOR_CLOUDS, COLOR_CLOUDS)
+
+    def showGameScene(self):
+        if self.didSnakeGoOffScreen(self.snake):
+            self.snake.loopBack()
+        else:
+            self.snake.moveHead(self.snake.xVelocity, self.snake.yVelocity)
+
+        self.snake.createTail()
+
+        self.screen.initBackground()
+        self.screen.draw().apple(self.apple.x, self.apple.y)
+        self.screen.draw().snake(self.snake)
+        self.screen.update()
+
+        if self.didSnakeCollideWithAnApple(self.snake, self.apple):
+            self.removeApple()
+            self.generateNewApple()
+            self.snake.incrementLength()
+            self.updateHighscore()
+
+        if self.inDebugMode():
+            if self.config['showGrid']:
+                self.screen.draw().grid()
+            self.showDebugMessage()
+
+        self.showScore(self.snake.length)
+        self.showHighScore(self.highscore)
+
+        if self.didSnakeCollideWithItself(self.snake):
+            self.end()
+
+        self.clock.tick(FRAMERATE)
+
+    # ---
+    # Setters and getters
+    # ---
+
     def pause(self):
         self.scene = PAUSE_SCENE
 
     def unpause(self):
         self.scene = GAME_SCENE
-
-    def showPauseScreen(self):
-        self.display.fill(COLOR_ASBESTOS)
-        self.message('Paused', 'Press ESC or space bar to continue... Q quits the game.', COLOR_CLOUDS, COLOR_CLOUDS)
-
-    def currentStyle(self, el):
-        return self.config['styles'][self.config['style']][el]
-
-    def font(self, size):
-        return self.pygame.font.SysFont(None, size)
-
-    def message(self, header, span, headerColor, spanColor):
-        header = self.font(40).render(header, True, headerColor)
-        span = self.font(25).render(span, True, spanColor)
-        self.display.blit(header, [WINDOW_WIDTH / 2 - header.get_rect().width / 2, WINDOW_HEIGHT / 2 - 15])
-        self.display.blit(span, [WINDOW_WIDTH / 2 - span.get_rect().width / 2, WINDOW_HEIGHT / 2 + 15])
-        self.pygame.display.update()
 
     def reset(self):
         self.scene = GAME_SCENE
@@ -87,36 +134,12 @@ class Game:
     def end(self):
         self.scene = GAME_OVER_SCENE
 
-    def updateHighscore(self):
-        if self.snake.length > self.highscore:
-            self.highscore = self.snake.length
-        open('db.txt', 'w').write('highscore=' + str(self.highscore))
-
-    def showGameOverScreen(self):
-        self.display.fill(COLOR_EMERALD)
-        self.message('Game Over (Your Score: ' + str(self.snake.length) + ')', 'Press ESC to quit or space bar to continue...', COLOR_CLOUDS, COLOR_CLOUDS)
-
-    def showDebugMessage(self):
-        debugMessage = self.font(15).render('In Debug Mode', True, COLOR_CLOUDS)
-        self.display.blit(debugMessage, [WINDOW_WIDTH - debugMessage.get_rect().width, WINDOW_HEIGHT - debugMessage.get_rect().height])
-        self.pygame.display.update()
-
-    def showScore(self, score):
-        debugMessage = self.font(20).render('Current length: ' + str(score), True, COLOR_CLOUDS)
-        self.display.blit(debugMessage, [10, 10])
-        self.pygame.display.update()
-
-    def showHighScore(self, score):
-        highscoreMessage = self.font(20).render('Highscore: ' + str(score), True, COLOR_CLOUDS)
-        self.display.blit(highscoreMessage, [WINDOW_WIDTH - highscoreMessage.get_rect().width - 10, 10])
-        self.pygame.display.update()
-
     def generateNewApple(self):
         x = random.randrange(0, TILE_COUNT_X)
         y = random.randrange(0, TILE_COUNT_Y)
         apple = Apple(x, y, GRID_SIZE)
 
-        if self.handler.doesAppleOverlapSnake(apple, self.snake):
+        if self.doesAppleOverlapSnake(apple, self.snake):
             self.generateNewApple()
         else:
             self.apple = apple
@@ -139,45 +162,82 @@ class Game:
     def setDebug(self, value):
         self.config['debug'] = value
 
-    def run(self):
-        self.generateNewApple()
+    def currentStyle(self, el):
+        return self.config['styles'][self.config['style']][el]
 
-        while True:
-            for event in self.pygame.event.get():
-                Event(self, event).handle(self.scene)
+    def font(self, size):
+        return self.pygame.font.SysFont(None, size)
 
-            if self.scene == GAME_OVER_SCENE:
-                self.showGameOverScreen()
-            elif self.scene == PAUSE_SCENE:
-                self.showPauseScreen()
-            elif self.scene == GAME_SCENE:
-                if self.handler.didSnakeGoOffScreen(self.snake):
-                    self.snake.loopBack()
-                else:
-                    self.snake.moveHead(self.snake.xVelocity, self.snake.yVelocity)
+    # ---
+    # Actions
+    # ---
 
-                self.snake.createTail()
+    def quit(self):
+        self.pygame.quit()
+        quit()
 
-                self.screen.initBackground()
-                self.screen.draw().apple(self.apple.x, self.apple.y)
-                self.screen.draw().snake(self.snake)
-                self.screen.update()
+    def moveSnake(self, key):
+        if self.snake.direction != 1:
+            if key == K_LEFT:
+                self.snake.turnLeft()
+        if self.snake.direction != 3:
+            if key == K_RIGHT:
+                self.snake.turnRight()
+        if self.snake.direction != 0:
+            if key == K_UP:
+                self.snake.turnUp()
+        if self.snake.direction != 2:
+            if key == K_DOWN:
+                self.snake.turnDown()
 
-                if self.handler.didSnakeCollideWithAnApple(self.snake, self.apple):
-                    self.removeApple()
-                    self.generateNewApple()
-                    self.snake.incrementLength()
-                    self.updateHighscore()
+    def didSnakeGoOffScreen(self, snake):
+        return (snake.x > TILE_COUNT_X - 1
+            or snake.x < 0
+            or snake.y > TILE_COUNT_Y - 1
+            or snake.y < 0)
 
-                if self.inDebugMode():
-                    if self.config['showGrid']:
-                        self.screen.draw().grid()
-                    self.showDebugMessage()
+    def didSnakeCollideWithAnApple(self, snake, apple):
+        return snake.x == apple.x and snake.y == apple.y
 
-                self.showScore(self.snake.length)
-                self.showHighScore(self.highscore)
+    def didSnakeCollideWithItself(self, snake):
+        for segment in snake.tail[:-1]:
+            if segment[0] == snake.x and segment[1] == snake.y:
+                return True
+        return False
 
-                if self.handler.didSnakeCollideWithItself(self.snake):
-                    self.end()
+    def doesAppleOverlapSnake(self, apple, snake):
+        for segment in snake.tail[:-1]:
+            if segment[0] == apple.x and segment[1] == apple.y:
+                return True
+        return False
 
-                self.clock.tick(FRAMERATE)
+    # ---
+    # Messaging
+    # ---
+
+    def message(self, header, span, headerColor, spanColor):
+        header = self.font(40).render(header, True, headerColor)
+        span = self.font(25).render(span, True, spanColor)
+        self.display.blit(header, [WINDOW_WIDTH / 2 - header.get_rect().width / 2, WINDOW_HEIGHT / 2 - 15])
+        self.display.blit(span, [WINDOW_WIDTH / 2 - span.get_rect().width / 2, WINDOW_HEIGHT / 2 + 15])
+        self.pygame.display.update()
+
+    def showDebugMessage(self):
+        debugMessage = self.font(15).render('In Debug Mode', True, COLOR_CLOUDS)
+        self.display.blit(debugMessage, [WINDOW_WIDTH - debugMessage.get_rect().width, WINDOW_HEIGHT - debugMessage.get_rect().height])
+        self.pygame.display.update()
+
+    def showScore(self, score):
+        debugMessage = self.font(20).render('Current length: ' + str(score), True, COLOR_CLOUDS)
+        self.display.blit(debugMessage, [10, 10])
+        self.pygame.display.update()
+
+    def showHighScore(self, score):
+        highscoreMessage = self.font(20).render('Highscore: ' + str(score), True, COLOR_CLOUDS)
+        self.display.blit(highscoreMessage, [WINDOW_WIDTH - highscoreMessage.get_rect().width - 10, 10])
+        self.pygame.display.update()
+
+    def updateHighscore(self):
+        if self.snake.length > self.highscore:
+            self.highscore = self.snake.length
+        open('db.txt', 'w').write('highscore=' + str(self.highscore))

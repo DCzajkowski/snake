593fbaa5e3cfc4e7d27eed6d2dc482b537f8d6d6
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-10-04 14:38:13 +0200

    Remove option for mode

--- App/Scenes/Settings.py
+++ App/Scenes/Settings.py
@@ -3,7 +3,7 @@ from config import *
 class Settings:
     game = None
     position = 0
-    elementsCount = 4
+    elementsCount = 3
 
     def __init__(self, game):
         self.game = game
@@ -21,7 +21,7 @@ class Settings:
             self.position = self.elementsCount - 1
 
     def enter(self):
-        if self.position == 3:
+        if self.position == 2:
             self.game.scene = MENU_SCENE
 
     def change(self, direction):
@@ -29,8 +29,8 @@ class Settings:
             self.toggleSpeed(direction)
         if self.position == 1:
             self.toggleStyle(direction)
-        if self.position == 2:
-            self.toggleMode(direction)
+        # if self.position == 2:
+        #     self.toggleMode(direction)
 
     def toggleSpeed(self, direction):
         value = AVAILABLE_SPEEDS[self.calculatePosition(direction, (AVAILABLE_SPEEDS).index(self.game.config['framerate']), len(AVAILABLE_SPEEDS))]
@@ -42,10 +42,10 @@ class Settings:
         self.game.config['style'] = value
         self.game.db.change('style', value)
 
-    def toggleMode(self, direction):
-        value = AVAILABLE_MODES[self.calculatePosition(direction, (AVAILABLE_MODES).index(self.game.config['mode']), len(AVAILABLE_MODES))]
-        self.game.config['mode'] = value
-        self.game.db.change('mode', value)
+    # def toggleMode(self, direction):
+    #     value = AVAILABLE_MODES[self.calculatePosition(direction, (AVAILABLE_MODES).index(self.game.config['mode']), len(AVAILABLE_MODES))]
+    #     self.game.config['mode'] = value
+    #     self.game.db.change('mode', value)
 
     def calculatePosition(self, direction, position, length):
         if direction == 1:
@@ -66,10 +66,10 @@ class Settings:
         text = self.game.font(30).render('Theme: ' + style, True, COLOR_CLOUDS)
         self.game.display.blit(text, [WINDOW_WIDTH / 2 - text.get_rect().width / 2, 200 + 40])
 
-        text = self.game.font(30).render('Mode: ' + str(self.game.config['mode']), True, COLOR_CLOUDS)
-        self.game.display.blit(text, [WINDOW_WIDTH / 2 - text.get_rect().width / 2, 200 + 80])
+        # text = self.game.font(30).render('Mode: ' + str(self.game.config['mode']), True, COLOR_CLOUDS)
+        # self.game.display.blit(text, [WINDOW_WIDTH / 2 - text.get_rect().width / 2, 200 + 80])
 
         text = self.game.font(30).render('Back to Menu', True, COLOR_CLOUDS)
-        self.game.display.blit(text, [WINDOW_WIDTH / 2 - text.get_rect().width / 2, 200 + 120])
+        self.game.display.blit(text, [WINDOW_WIDTH / 2 - text.get_rect().width / 2, 200 + 80])
 
         self.game.display.blit(self.game.images['menu-selection'], [WINDOW_WIDTH / 2 - self.game.images['menu-selection'].get_rect().width / 2 - 120, 198 + (40 * self.position)])
